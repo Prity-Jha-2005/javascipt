b95d07d5b34d0cc2e30914c45c4ef2b4d66581d9
Prity-Jha-2005 <ppjj20775@gmail.com>
2026-06-15 13:34:36 +0000

    container config done

---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+const accountName = 12456
+var accountId = "1bi23ri037"
+let accountBranch = "robotics and AI"
+ var accountemail = "prityjhapkr22@gmail.com"
+ accounthobby = "singing"
+/*
+pefere not to use let
+*/ 
+  
+console.table([accountName,accountId, accountemail,accounthobby,accountBranch])
+  
