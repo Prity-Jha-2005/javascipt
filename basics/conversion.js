c64e83643d9c14a915fdc67d95fc9983a01ebb41
Prity-Jha-2005 <ppjj20775@gmail.com>
2026-06-16 21:28:40 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,14 @@
+
+"use strict"
+let name="Prity jha"
+let spend =50000
+
+let usn= "1bi23ri037"  // ye pura number + vharecter hai isliye isko "" iske andr rakhenge
+let college = "BIT"
+let boolean = null // null is a object.
+
+
+let valueInNumber = Number(usn)
+console.log(valueInNumber);
+let spendInNumber = Number(spend)
+console.log(spendInNumber);
---
+++ basics/datatype.js
@@ -0,0 +1,14 @@
+"use strict"
+let name="Prity jha"
+
+let usn= "1bi23ri037"  // ye pura number + vharecter hai isliye isko "" iske andr rakhenge
+let college = "BIT"
+let boolean = null // null is a object.
+console.log(college)
+console.log(boolean)
+let placement
+console.log(placement)
+
+let valueInNumber = number(usn)
+console.log(valueInNumber);
+
